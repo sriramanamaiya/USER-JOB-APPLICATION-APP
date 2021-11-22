ad7757ca8a1dee64565595a3d05d26dafce14461
sriramana <sriramanamaiya99@gmail.com>
2021-11-22 12:18:18 +0530

    made changes in button comp

--- src/admin-dashboard/components/ButtonComp.js
+++ src/admin-dashboard/components/ButtonComp.js
@@ -5,8 +5,12 @@ const ButtonComp = (props) => {
     const { handleClick, developer } = props
 
     return (
-        <Button variant="outline-primary" className="me-2 mb-1" name={developer} onClick={handleClick}>{developer}</Button>
-        
+        <Button 
+            variant="outline-primary" 
+            className="me-2 mb-1" 
+            name={developer} 
+            onClick={handleClick}
+        >{developer}</Button>
     )
 }
 
